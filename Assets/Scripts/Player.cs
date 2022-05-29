c9f1af38438460d9f6312ad7a2c27d411eec3f3e
BigBrainUcn <bigbrainsucn@gmail.com>
2022-05-29 13:10:56 -0400

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float playerVelocity;
+    private float jumpForce;
+    private bool isJumping;
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Rigidbody2D playerRigidBody;
+
+    private void OnEnable()
+    {
+        this.animator = GetComponent<Animator>();
+        this.spriteRenderer = GetComponent<SpriteRenderer>();
+        this.playerRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void AttributesInitialization()
+    {
+        this.playerVelocity = 0.1f;
+        this.jumpForce = 9f;
+        this.isJumping = true;
+    }
+
+    private void Start()
+    {
+        AttributesInitialization();
+    }
+
+    private void Update()
+    {
+        this.WalkAnimations();
+        this.PlayerJump();
+    }
+
+    void PlayerMovements()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(playerVelocity, 0f, 0f);
+            spriteRenderer.flipX = false;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-playerVelocity, 0f, 0f);
+            spriteRenderer.flipX = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        this.PlayerMovements();
+    }
+  
+    void PlayerJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            playerRigidBody.AddForce(new Vector3(0f, jumpForce, 0f), ForceMode2D.Impulse);
+        }
+    }
+
+    void WalkAnimations()
+    {
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.A) && !isJumping)
+        {
+            animator.SetBool("isWalk", true);
+        }
+        if (!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A) && !isJumping)
+        {
+            animator.SetBool("isWalk", false);
+
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            this.isJumping = true;
+            animator.SetBool("isJump", true);
+            animator.SetBool("isWalk", false);
+        }
+
+        if (other.gameObject.CompareTag("Wc"))
+        {
+            this.isJumping = true;
+            animator.SetBool("isJump", true);
+            animator.SetBool("isWalk", false);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            this.isJumping = false;
+            animator.SetBool("isJump", false);
+        }
+
+        if (other.gameObject.CompareTag("Wc"))
+        {
+            this.isJumping = false;
+            animator.SetBool("isJump", false);
+        }
+    }
+}
